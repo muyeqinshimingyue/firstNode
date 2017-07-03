9464ab06c16bf4053458d9b00b1ebe8f7fc2b029
muyeqinshimingyue <1574862254@qq.com>
2017-07-03 12:02:35 +0800

    测试  覆盖 exports公共接口

---
+++ test/hello.js
@@ -0,0 +1,29 @@
+/**
+ *  测试Node.js模块   hello 模块  
+ *  
+ *  覆盖exports对象，
+ *  因为exports对象是Node.js模块的公共接口，所以模块都会有一个exports的接口，
+ *  现在定义一个模块直接覆盖exports对象，让外部调用方法直接获取模块定义的对象
+ *  
+ *  
+ *  exports  本身仅仅是一个普通的空对象，即 {} ，它专门用来声明接口，本质上是
+ *  通过它为模块闭包① 的内部建立了一个有限的访问接口。因为它没有任何特殊的地方，所以
+ *  可以用其他东西来代替
+ */
+
+// 定义对象
+function Hello(){
+	
+	var name;  // 对象属性
+	
+	this.setName = function ( username){
+		name = username;
+	};
+	
+	this.sayHello = function (){
+		console.log( "hello " + name );
+	};
+	
+}
+
+module.exports= Hello;
---
+++ test/test_hello.js
@@ -0,0 +1,17 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+// 获取js 模块
+var Hello = require("./hello.js");
+
+// 实例化对象
+he = new Hello();
+he2 = new Hello();
+
+he.setName("abc");
+he2.setName("123123")
+
+
+he.sayHello();
+he2.sayHello();
